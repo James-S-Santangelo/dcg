454d1dae5fe3b171d47e52cb0c5ec812141827b9
James Santangelo <james.santangelo37@gmail.com>
2023-02-14 13:10:34 -0800

    Minor tweak to sync script

--- scripts/rsync/cedar_to_hpcnode.sh
+++ scripts/rsync/cedar_to_hpcnode.sh
@@ -1,3 +1,4 @@
-rsync -vuar -P \
+rsync -vuar \
+    --dry-run \
     santang3@cedar.computecanada.ca:/home/santang3/scratch/dovetail/results/ \
     /research/projects/trifolium/dovetail/dcg/results 
